ff315f8c15a918814368a0f464b6ccb3c99851d1
Julianrvillamil <julianrvillamil@gmail.com>
2025-10-23 00:06:38 -0500

    feat: Añadir configuración de símbolos materiales y actualizar enlace de fuente

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="es" className="dark">
       <head>
         <link 
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=optional" 
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap" 
           rel="stylesheet" 
         />
       </head>
